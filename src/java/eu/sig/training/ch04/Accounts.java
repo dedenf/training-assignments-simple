05d0e07c12bfdf175f0343c903e7c40d852657c8
Deden Fathurahman <dedenf@gmail.com>
2017-01-19 14:42:35 +0700

    testing new refactoring

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -1,6 +1,8 @@
 package eu.sig.training.ch04;
 
 public class Accounts {
+    private Money balance = new Money();
+    
     @SuppressWarnings("unused")
     public static CheckingAccount findAcctByNumber(String number) {
         return new CheckingAccount();
@@ -15,4 +17,31 @@ public class Accounts {
         return sum % 11 == 0;
     }
     // end::isValid[]
+
+    public static bankAccountCheck(String counterAccount){
+        // 2. Assuming result is 9-digit bank account number, validate 11-test:
+        int sum = 0;
+        for (int i = 0; i < counterAccount.length(); i++) {
+            char character = counterAccount.charAt(i);
+            int characterValue = Character.getNumericValue(character);
+            sum = sum + (9 - i) * characterValue;
+        }
+        if (sum % 11 == 0) {
+            // 3. Look up counter account and make transfer object:
+            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+            Transfer result = new Transfer(this, acct, amount);
+            return result;
+        } else {
+            throw new BusinessException("Invalid account number!");
+        }
+    }
+
+    public void addInterest() {
+        Money interest = balance.multiply(INTEREST_PERCENTAGE);
+        if (interest.greaterThan(0)) {
+            balance.add(interest);
+        } else {
+            balance.substract(interest);
+        }
+    }
 }
